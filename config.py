a7cdbfa60ef3a99e68ad058f5e968a9584c6651e
xerikcaro <erikalopez1215cm@gmail.com>
2025-06-16 15:49:20 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ class Config:
     MYSQL_USER          = os.getenv("MYSQL_USER")
     MYSQL_PASSWORD      = os.getenv("MYSQL_PASSWORD")
     MYSQL_DB            = os.getenv("MYSQL_DB")
-    MYSQL_PORT = int(os.getenv("MYSQL_PORT"))
+    MYSQL_PORT          = int(os.getenv("MYSQL_PORT"))
